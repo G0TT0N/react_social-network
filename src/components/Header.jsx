343dedb588d478f0bb64c34a1529226d03062f51
G0TT0N <gotton92@mail.ru>
2019-07-17 15:43:01 +0300

    load project

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src="https://about.canva.com/wp-content/uploads/sites/3/2016/08/logos-1.png" alt="logo"
+                 width="100px"/>
+        </header>
+    )
+};
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img
+                    src="https://images.unsplash.com/photo-1500382017468-9049fed747ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+                    alt=""/>
+            </div>
+            <div>
+                ava + desc
+            </div>
+            <div>
+                my posts
+                <div>
+                    new post
+                </div>
+                <div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default Profile
