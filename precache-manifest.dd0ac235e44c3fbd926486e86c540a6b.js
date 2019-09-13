f67557bb08992e61a67f41e161bef8e4116bebdf
G0TT0N <gotton92@mail.ru>
2019-09-13 17:47:04 +0300

    Updates

---
+++ precache-manifest.dd0ac235e44c3fbd926486e86c540a6b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c20b6f863ea300377430ea69e8ac94a",
+    "url": "/react_social-network/index.html"
+  },
+  {
+    "revision": "54fa384238775a582e94",
+    "url": "/react_social-network/static/css/3.092fd9ca.chunk.css"
+  },
+  {
+    "revision": "23e2e362058b8b6d490e",
+    "url": "/react_social-network/static/css/4.66f6ef65.chunk.css"
+  },
+  {
+    "revision": "198fb5133752a950d76e",
+    "url": "/react_social-network/static/css/main.756ced14.chunk.css"
+  },
+  {
+    "revision": "39da019eccd4d10e0a4f",
+    "url": "/react_social-network/static/js/2.a3d589bd.chunk.js"
+  },
+  {
+    "revision": "54fa384238775a582e94",
+    "url": "/react_social-network/static/js/3.873571ac.chunk.js"
+  },
+  {
+    "revision": "23e2e362058b8b6d490e",
+    "url": "/react_social-network/static/js/4.eba13770.chunk.js"
+  },
+  {
+    "revision": "198fb5133752a950d76e",
+    "url": "/react_social-network/static/js/main.bad74163.chunk.js"
+  },
+  {
+    "revision": "05f1c3093cee6ad9b65c",
+    "url": "/react_social-network/static/js/runtime~main.b76a634f.js"
+  },
+  {
+    "revision": "52e4201e15b601039a0306a40b5f49c8",
+    "url": "/react_social-network/static/media/User.52e4201e.png"
+  },
+  {
+    "revision": "3ca1c9118bcd3b3799eacb857b87dcec",
+    "url": "/react_social-network/static/media/loader.3ca1c911.svg"
+  }
+]);
